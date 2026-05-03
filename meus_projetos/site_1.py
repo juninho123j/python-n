14c3d073afa631b2d25158dd660269ae5e3f2b3e
juninho123 <robsonmoraesjuniortt@gmail.com>
2026-05-03 15:48:25 -0300

    Update site_1.py

--- meus_projetos/site_1.py
+++ meus_projetos/site_1.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__, template_folder='template')
 
@@ -6,9 +6,10 @@ app = Flask(__name__, template_folder='template')
 def login():
     return render_template('login.html')
 
-@app.route('/inicio')
+@app.route('/inicio', methods=['POST'])
 def homepage():
-    return render_template('homepage.html')
+    texto = request.form['usuario']
+    return render_template('homepage.html', resposta=texto)
 
 @app.route('/usuario')
 def usuario():
