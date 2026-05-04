71e640f9e7bfc53c515235139e17ec555f549e98
juninho123 <robsonmoraesjuniortt@gmail.com>
2026-05-04 13:29:08 -0300

    Create ex_logica2.py

---
+++ aulas/ex_logica2.py
@@ -0,0 +1,9 @@
+num1 = int(input('digite um numero :'))
+num2 = int(input('digite outro numero :'))
+
+if num1 >= num2:
+    print(f'o numero {num1} é o maior')
+    print(f'o numero {num2} é o menor')
+else:
+    print(f'o numero {num2} é o maior')
+    print(f'o numero {num1} é o menor ')
