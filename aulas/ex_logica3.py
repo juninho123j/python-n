9c00bff8df1d11ad3a4d061590c04565321ce420
juninho123 <robsonmoraesjuniortt@gmail.com>
2026-05-04 16:19:21 -0300

    Create ex_logica3.py

---
+++ aulas/ex_logica3.py
@@ -0,0 +1,11 @@
+num1 = int(input('digite a nota do aluno :'))
+num2 = int(input('digite a nota do aluno :'))
+num3 = int(input('digite a nota do aluno :'))
+
+result = (num1 + num2 + num3)/3
+
+if result >= 7:
+    print('aprovado')
+else:
+    print('reprovado')
+    
