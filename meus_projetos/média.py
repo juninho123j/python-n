e96ba957df53dcb6456c4b281b32d33865ef1cdb
juninho123 <robsonmoraesjuniortt@gmail.com>
2026-07-05 12:06:22 -0300

    Delete média.py

--- meus_projetos/média.py
+++
@@ -1,9 +0,0 @@
-nota1 = float(input('escreva uma nota ? '))
-nota2 = float(input('escreva outra nota ?'))
-valor = nota1 + nota2/2
-
-if valor >=7:
-    print('aprovado')
-else:
-    print('reprovado')
-    
